052705151fd1af72a36c6882f7c3a0c4a29a5a1b
Rohan Sidhu <102923106+innovativeroh@users.noreply.github.com>
2024-04-10 22:23:26 +0530

    Core Files

---
+++ includes/header.php
@@ -0,0 +1,1 @@
+<link rel="stylesheet" href="./core/css/main.css" type="text/css">
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("./includes/header.php"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
